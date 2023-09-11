5c7847a210bdb12081c484d82d2fb02ba15ecd7c
rafizia <mohrafiziaulhaq@gmail.com>
2023-09-11 14:38:21 +0700

    update main.html

--- main/views.py
+++ main/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'name': 'Muhammad Rafi Zia Ulhaq',
-        'class': 'PBP B'
+        'class': 'PBP B',
+
+        'weapon1': 'Sword of Descensions'
     }
 
     return render(request, "main.html", context)
