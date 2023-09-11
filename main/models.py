b2d11c35d364f268e5be44597720d7c57f0679f0
rafizia <mohrafiziaulhaq@gmail.com>
2023-09-11 19:26:15 +0700

    menambahkan atribut pada models.py, menambah context pada views.py, dan update template main.html

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
-class Product(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
-    price = models.IntegerField()
+    type = models.CharField(max_length=255)
+    atk = models.IntegerField()
+    rarity = models.CharField(max_length=255)
     description = models.TextField()
+    amount = models.IntegerField()
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
+        'app-name' : 'Weapentory - Ultimate Weapon Inventory',
         'name': 'Muhammad Rafi Zia Ulhaq',
         'class': 'PBP B',
 
